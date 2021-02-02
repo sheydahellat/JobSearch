98878ac38ce234adf625138df3c8b981afaa2c4f
sheyda <sheydahellat@gmail.com>
2021-02-02 16:53:52 +0330

    add user

---
+++ JobSearch/src/User.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class User {
+
+    public string UserName;
+    public string Password;
+    public User(string userName,string passWord){
+        this.UserName=userName;
+        this.Password=passWord;
+
+    }
+
+
+}
